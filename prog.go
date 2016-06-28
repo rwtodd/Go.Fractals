6aa82635660d57c1a736e1f4049a53beb8c257ec
Richard Todd <richard@waywardcode.com>
2016-06-28 15:46:45 -0500

    got palette issue sorted out

--- prog.go
+++ prog.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-  "fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"os"
 
-  "github.com/rwtodd/fractals-go/algo"
+	"github.com/rwtodd/fractals-go/algo"
 )
 
+const (
+	picwidth  = 600
+	picheight = 600
+)
+
+type fractalState struct {
+	midx, midy, spanx, spany float64
+}
+
+func gray256() color.Palette {
+	ans := make(color.Palette, 256)
+	for i := range ans {
+		ans[i] = color.Gray{uint8(i)}
+	}
+	return ans
+}
 
 func main() {
-  f := algo.NewMandelbrot(500)
-  var x, y float64
-  for y = -1 ; y < 1 ; y += 0.1 {
-  for x = -1 ; x < 1 ; x += 0.1 {
-     fmt.Printf("At %v, %v = %v\n", x, y, f.Intensity(x,y)) 
-  }
-     fmt.Printf("\n")
-  }
+	f := algo.NewMandelbrot(256)
+	fs := fractalState{midx: 0, midy: 0, spanx: 2, spany: 2}
+
+	img := image.NewPaletted(image.Rectangle{image.Pt(0, 0),
+		image.Pt(picwidth, picheight)},
+		gray256())
+
+	xUL := fs.midx - 0.5*fs.spanx
+	yUL := fs.midy - 0.5*fs.spany
+	for y := 0; y < picheight; y++ {
+		ycoord := yUL + float64(y)*fs.spany/float64(picheight)
+		for x := 0; x < picwidth; x++ {
+			xcoord := xUL + float64(x)*fs.spanx/float64(picwidth)
+			img.SetColorIndex(x, y, f.Intensity(xcoord, ycoord))
+		}
+	}
+
+	gif.Encode(os.Stdout, img, nil)
 }
