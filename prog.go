6e0448b5183403a41c5238ea159e6d032b5bc4d3
Richard Todd <richard@waywardcode.com>
2016-06-28 16:12:44 -0500

    made drawing concurrent

--- prog.go
+++ prog.go
@@ -5,6 +5,8 @@ import (
 	"image/color"
 	"image/gif"
 	"os"
+	"runtime"
+	"sync"
 
 	"github.com/rwtodd/fractals-go/algo"
 )
@@ -26,26 +28,48 @@ func gray256() color.Palette {
 	return ans
 }
 
-func drawImage(fract algo.Fractal, fs *fractalState) image.Image {
-	img := image.NewPaletted(image.Rectangle{image.Pt(0, 0),
-		image.Pt(picwidth, picheight)},
-		gray256())
-
+func drawFractal(fract algo.Fractal, 
+		fs *fractalState, 
+		img *image.Paletted, 
+		stY int, 
+		endY int) {
 	xUL := fs.midx - 0.5*fs.spanx
 	yUL := fs.midy - 0.5*fs.spany
-	for y := 0; y < picheight; y++ {
+	for y := stY; y < endY; y++ {
 		ycoord := yUL + float64(y)*fs.spany/float64(picheight)
 		for x := 0; x < picwidth; x++ {
 			xcoord := xUL + float64(x)*fs.spanx/float64(picwidth)
 			img.SetColorIndex(x, y, fract.Intensity(xcoord, ycoord))
 		}
 	}
+}
+
+func drawImage(fract algo.Fractal, fs fractalState) image.Image {
+	var wg sync.WaitGroup
+	img := image.NewPaletted(image.Rectangle{image.Pt(0, 0),
+		image.Pt(picwidth, picheight)},
+		gray256())
+	
+	divisions := runtime.NumCPU()
+        divSize := picheight / divisions 
+
+	wg.Add(divisions)
+        for div := 0; div < divisions; div++ {
+		go func(div int) {
+	 		endLine := (div+1)*divSize
+			if div == (divisions-1) { endLine = picheight }
+		        drawFractal(fract, &fs, img, div*divSize, endLine)
+			wg.Done()
+		}(div)
+	}
+
+	wg.Wait()
 	return img
 }
 
 func main() {
 	f := algo.NewMandelbrot(256)
 	fs := fractalState{midx: 0, midy: 0, spanx: 2, spany: 2}
-	img := drawImage(f, &fs)
+	img := drawImage(f, fs)
 	gif.Encode(os.Stdout, img, nil)
 }
