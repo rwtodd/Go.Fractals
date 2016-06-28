fdcfa7f255d741cd83b8c20ebc4983d16db82d53
Richard Todd <richard@waywardcode.com>
2016-06-28 13:26:47 -0500

    gofmt and added norm()

--- algo/fractal.go
+++ algo/fractal.go
@@ -4,26 +4,29 @@
 package algo
 
 import (
-  "fmt"
+	"fmt"
 )
 
 // Fractal is the interface that decouples the driver program
 // from the particular fractal being rendered.
 type Fractal interface {
-   fmt.Stringer
+	fmt.Stringer
 
-   // Intensity determines the pixel value of the given
-   // coordinates.
-   Intensity(x,y float64) uint8
+	// Intensity determines the pixel value of the given
+	// coordinates.
+	Intensity(x, y float64) uint8
 
-   // Name gives the name of the fractal equation/algorithm.
-   Name() string
- 
-   // ArgHelp provides a help string for use in the UI,
-   // telling the user what the arguments to that fractal
-   // represent.
-   ArgHelp() string
+	// Name gives the name of the fractal equation/algorithm.
+	Name() string
 
+	// ArgHelp provides a help string for use in the UI,
+	// telling the user what the arguments to that fractal
+	// represent.
+	ArgHelp() string
 }
 
-
+// Norm computes the norm of a complex variable
+func norm(x complex128) float64 {
+	rl, im := real(x), imag(x)
+	return rl*rl + im*im
+}
--- algo/mandelbrot.go
+++ algo/mandelbrot.go
@@ -1,42 +1,41 @@
 package algo
 
 import (
-   "fmt"
+	"fmt"
 )
 
 type mandel struct {
-   depth int
+	depth int
 }
 
-
 func (f mandel) Name() string {
-   return "Mandelbrot"
+	return "Mandelbrot"
 }
 
 func (f mandel) String() string {
-   return fmt.Sprintf("Mandelbrot (max depth %d)", f.depth)
+	return fmt.Sprintf("Mandelbrot (max depth %d)", f.depth)
 }
 
 func (f mandel) ArgHelp() string {
-   return "No arguments."
+	return "No arguments."
 }
 
-func (f mandel) Intensity(x,y float64) uint8 {
-   pos := complex(x,y)
-   val := pos
-   ans := f.depth - 1
+func (f mandel) Intensity(x, y float64) uint8 {
+	pos := complex(x, y)
+	val := pos
+	ans := f.depth - 1
 
-   for ans = f.depth - 1 ; ans > 0  ; ans-- {
-      if real(val)*real(val)+imag(val)*imag(val) >= 4.0 {
-         break
-      }
-      val = val*val + pos
-   }
+	for ans = f.depth - 1; ans > 0; ans-- {
+		if norm(val) >= 4.0 {
+			break
+		}
+		val = val*val + pos
+	}
 
-   // scale the answer to the 0 - 255 range
-   return uint8(float64(ans)*256 / float64(f.depth))
+	// scale the answer to the 0 - 255 range
+	return uint8(float64(ans) * 256 / float64(f.depth))
 }
 
 func NewMandelbrot(depth int) Fractal {
-  return mandel{depth}
+	return mandel{depth}
 }
